7ca7605f9417dabbfa367f6de9677a77aaf85bec
estatemaster100 <estatemaster100@gmail.com>
2025-11-20 12:15:23 +0100

    Fixed Security list not displaying

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "404915119"
+    serviceWorkerVersion: "340037871"
   }
 });
