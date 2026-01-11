3011c89b0089fd3caf1977cdd43295481dd3abe1
estatemaster100 <estatemaster100@gmail.com>
2026-01-11 17:28:16 +0100

    adjusted admin settings to tiles

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "760222068"
+    serviceWorkerVersion: "2601492665"
   }
 });
